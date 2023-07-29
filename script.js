12f753df28646f37ca44747af38f0b886808d3f0
Jules Oreste Mpano <mpanojulesoreste@gmail.com>
2023-07-29 00:35:52 -0400

    Introductory web development for the website

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript code for handling map interactivity can be added here.
+// For example, you can add click event listeners to specific regions on the map.
+// This is just a placeholder for demonstration purposes.
+const usMap = document.getElementById("usMap");
+usMap.addEventListener("click", function() {
+  alert("You clicked on the United States map!");
+});
